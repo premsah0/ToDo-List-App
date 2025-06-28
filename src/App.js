d3c4bf58523d2f6e51f35f16b27d1b655734988d
Prem Sah <premsah@Prems-MacBook-Air.local>
2025-06-28 23:36:33 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
 import Todoinput from './components/TodoInput'
+import TodoList from './components/TodoList';
 function App() {
+  const [listTodo,setListTodo]=useState([]);
+  let addList = (inputText)=>{
+    if(inputText!=="")
+    setListTodo([...listTodo,inputText]);
+  }
+  const deleteListItem = (key)=>{
+    let newListTodo = [...listTodo]
+    newListTodo.splice(key,1)
+    setListTodo([...newListTodo])
+  }
   return (
     <div className="main-container">
       <div className="center-container">
-        <Todoinput/>
+        <Todoinput addList={addList}/>
+        <h1 className="app-heading">TODO</h1>
+        <hr/>
+        {listTodo.map((listItem,i)=>{
+          return(
+            <TodoList key={i} index={i} item={listItem} deleteItem = {deleteListItem}/>
+          )
+
+          
+          
+        })}
       </div>
 
     </div>
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Todoinput() {
+function Todoinput(props) {
+    const[inputText,setInputText]=useState('');
+    const handleEnterPress = (e)=>{
+        if(e.keyCode===13){
+            props.addList(inputText)
+            setInputText("")
+        }
+    }
   return (
-    <div>
-      hbib
+    <div className="input-container">
+        <input type="text" className='input-box-todo' placeholder='enter your todo' value={inputText} onChange={e=>{
+            setInputText(e.target.value)
+        }}
+        onKeyDown={handleEnterPress}
+        />
+        <button className='add-btn' onClick={()=>{
+            props.addList(inputText)
+            setInputText("")
+        }}>+</button>
+        {/* <div>{inputText}</div> */}
     </div>
   )
 }
